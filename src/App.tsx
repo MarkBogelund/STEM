01cf220996657737a394888c722340bd929c4db4
Mark Bøgelund <mark.bogelund@BoostPLM.com>
2023-11-14 19:49:24 +0100

    Logo anim

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,11 @@ function App() {
 
   return (
     <div className="w-full bg-light">
+      <div className="fixed top-0 right-0 z-10 flex mr-4 mt-4 gap-2">
+        <div className="w-14 aspect-square flex justify-center items-center text-white"></div>
+        <div className="w-14 aspect-square flex justify-center items-center text-white"></div>
+      </div>
+
       <div className="top-0 w-full h-[35vh] bg-dark flex justify-center items-center">
         <Animation
           animationData={LogoAnimationData}
