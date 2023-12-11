7dba48acfd14fc785dcfae82707a1f9dfc7f5296
Mark Bøgelund <mark.bogelund@BoostPLM.com>
2023-12-11 15:10:54 +0100

    Updated institution logos

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,28 +1,15 @@
 import React from "react";
 import BannerVideo from "../videos/bannerVideoDemoDone.mp4";
-import ScienceLogo from "../images/science.svg";
-import TechnologyLogo from "../images/technology.svg";
-import EngineeringLogo from "../images/engineering.svg";
-import MathematicsLogo from "../images/mathematics.svg";
+import SDU_logo from "../images/SDU_logo.svg";
+import STEM_logo from "../images/STEM_logo.svg";
 
 const Banner: React.FC = () => {
   return (
     <>
-      <div className="fixed top-0 right-0 z-10 flex justify-center gap-2 h-[6vh]">
-        <div className="w-[14%] aspect-square fill-current flex justify-center items-center text-white">
-          <img src={ScienceLogo} alt="" />
-        </div>
-        <div className="w-[14%] aspect-square fill-current flex justify-center items-center text-white">
-          <img src={TechnologyLogo} alt="" />
-        </div>
-        <div className="w-[14%] aspect-square fill-current flex justify-center items-center text-white">
-          <img src={EngineeringLogo} alt="" />
-        </div>
-        <div className="w-[14%] aspect-square fill-current flex justify-center items-center text-white">
-          <img src={MathematicsLogo} alt="" />
-        </div>
+      <div className="fixed top-0 right-0 z-10 flex justify-center h-[6vh]">
+        <img src={STEM_logo} alt="" className="scale-[100%]" />
+        <img src={SDU_logo} alt="" className="scale-[100%]" />
       </div>
-
       <div className="relative w-full h-[60vh] bg-dark">
         <div className="absolute top-0 left-0 w-full h-full overflow-hidden">
           <video
